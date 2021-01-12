272db955d4e6a6b05f90edb296a7e9c9796ff6a9
Fujikuun <77313519+Fujikuun@users.noreply.github.com>
2021-01-12 19:06:03 +0900

    Add files via upload

---
+++ docker_simple_memo_php/common/auth.php
@@ -0,0 +1,38 @@
+<?php
+
+if(!isset($_SESSION)){
+    session_start();
+}
+
+//  ログインしているかチェックする
+//  @return bool
+ 
+function isLogin() {
+    if (isset($_SESSION['user'])) {
+        return true;
+    }
+
+    return false;
+}
+//ログインしているユーザーの表示名ユーザー名を取得
+
+function getLoginUserName() {
+  if (isset($_SESSION['user'])) {
+      $name = $_SESSION['user']['name'];
+
+      if (7 < mb_strlen($name)) {
+          $name = mb_substr($name, 0, 7) . "...";
+      }
+
+      return $name;
+  }
+
+  return "";
+}
+function getLoginUserId() {
+  if (isset($_SESSION['user'])) {
+      return $_SESSION['user']['id'];
+  }
+
+  return null;
+}
